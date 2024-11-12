44b69bb8a4cde60b21bd634e461143f3bf869673
Yash Sheth <yashsheth66@gmail.com>
2024-11-12 19:08:09 +0530

    factory constructor

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,19 @@ void main() {
   runApp(const MyApp());
 }
 
-// Named list of related items
-enum PersonProperties { firstName, lastName, age }
+class Cat {
+  final String name;
+  Cat(this.name);
+
+  // Factory Constructor, used when you are using an instance of a class with the same values multiple times in code
+  factory Cat.fluffball() {
+    return Cat('Fluff Ball');
+  }
+}
 
 void test() {
-  print(PersonProperties.firstName.name);
-  // use Switch case instead of if else over enumerations
+  final fluffBall = Cat.fluffball();
+  print(fluffBall.name);
 }
 
 class MyApp extends StatelessWidget {
