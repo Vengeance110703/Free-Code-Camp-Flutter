9a41afeebfdfcb5cbd1fab6a207e920dab5048de
Yash Sheth <yashsheth66@gmail.com>
2024-11-12 18:03:49 +0530

    multiply string

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,19 @@ void main() {
   runApp(const MyApp());
 }
 
+void test() {
+  // Multiply string is possible
+  final name = "Foo";
+  final nameTimes20 = name * 20;
+  print(nameTimes20);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
