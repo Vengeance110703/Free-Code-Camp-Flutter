46e08c6712f534be3d01cff386891d47943c1b16
Yash Sheth <yashsheth66@gmail.com>
2024-11-13 13:51:37 +0530

    add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
-class Pair<A, B> {
-  final A value1;
-  final B value2;
-
-  Pair(this.value1, this.value2);
-}
-
-void test() {
-  final names = Pair('foo', 20);
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
